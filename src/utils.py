13049114c96133ad70c502456ceee90500235f18
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-11-05 17:29:24 -0600

    began utils.py for plotting and such

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from typing import List
+
+def plot_loss(train_losses: List[int], test_losses: List[int], filename: str):
+    """Generate and save the plot of the performance
+
+    Args:
+        train_losses (List[int]): Training losses by the epoch
+        test_losses (List[int]): Testing losses by the epoch
+        filename (str): Name of the file to save the plotted results in
+    """
+    pass
