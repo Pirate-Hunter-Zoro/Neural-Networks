6fe9a136aa15f46e5c840c57569b133c396e6c6a
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-11-06 21:38:30 -0600

    skeleton of cnn utils

---
+++ src/cnn/utils.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+from typing import Dict
+
+def plot_loss_and_accuracy(results: Dict, filename: str):
+    """Helper function to plot the included loss and accuracy
+
+    Args:
+        results (Dict): Containing loss and accuracy
+        filename (str): Where to save the plot file
+    """
+    pass
